0724cc78f6a064de316a7ca561acbd9554a6c36f
PopPenguin2018 <poppen2018@gmail.com>
2019-02-06 19:05:34 -0800

    update

--- src/main/java/frc/robot/VisionController.java
+++ src/main/java/frc/robot/VisionController.java
@@ -13,7 +13,8 @@ public class VisionController implements RobotController{
    
     public VisionController(RobotProperties properties) {
         camera1 = CameraServer.getInstance().startAutomaticCapture(0);
-        camera1.setResolution(640, 480);
+        camera1.setResolution(320, 240);
+        camera1.setFrameRate(15);
         //camera2 = CameraServer.getInstance().startAutomaticCapture(1);
         //camera2.setResolution(640, 480);
 
