bd33741d42bfb4c4ccdea422eabf1f3056f5aea6
Ishan Madan <ishan@mistlogic.com>
2019-02-16 11:54:38 -0800

    I have no idea

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -51,7 +51,7 @@ public class Robot extends TimedRobot {
     camera.setResolution(640, 480);*/
     
   }
-
+  
   /**
    * This function is called every robot packet, no matter the mode. Use
    * this for items like diagnostics that you want ran during disabled,
