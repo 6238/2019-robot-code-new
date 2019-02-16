204385b4c543e69d0f79c6eaf3fe847a6c9dcd1b
Ishan Madan <ishan@mistlogic.com>
2019-02-16 11:54:09 -0800

    Removed unnecessary Indent

--- src/main/java/frc/robot/ElevatorController.java
+++ src/main/java/frc/robot/ElevatorController.java
@@ -1,17 +1,17 @@
 /*First way: map 1 button to the elevator and the number of times you press the button controls the elevator
-        first press: elevator moves up
-        second press: elevator stops
-        third press: elevator moves down
-        fourth press: elevator stops
+first press: elevator moves up
+second press: elevator stops
+third press: elevator moves down
+fourth press: elevator stops
 
-        Second way: map 2 buttons to the elevator
-        while button 1 is pressed: elevator moves up
-        while button 2 is pressed: elevator moves down
-        release both buttons to stop elevator
-        elevator does nothing if both buttons are pressed at the same time
+Second way: map 2 buttons to the elevator
+while button 1 is pressed: elevator moves up
+while button 2 is pressed: elevator moves down
+release both buttons to stop elevator
+elevator does nothing if both buttons are pressed at the same time
 
-        For both way, keep track of the position of the elevator, so you can tell it to stop moving if it has hit the top or the bottom.
-        */
+For both way, keep track of the position of the elevator, so you can tell it to stop moving if it has hit the top or the bottom.
+*/
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
