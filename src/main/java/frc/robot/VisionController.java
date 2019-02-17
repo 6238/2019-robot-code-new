ab6103a3eba186f793e03567e1fadaf9d8e3bc05
Ishan <ishan@mistlogic.com>
2019-02-17 13:29:08 -0800

    stuff

--- src/main/java/frc/robot/VisionController.java
+++ src/main/java/frc/robot/VisionController.java
@@ -11,61 +11,65 @@ import edu.wpi.first.wpilibj.vision.VisionThread;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 import java.util.*;
-public class VisionController implements RobotController{
-    private GripPipeline pipeline;
+
+public class VisionController implements RobotController {
+    private GripPipeline2 bwpipeline;
     private ArrayList<GripPipeline.Line> filteredLines;
     private UsbCamera camera1;
     private UsbCamera camera2;
     private CvSink cvSink;
     private VideoSink server;
     private Boolean prevButton = false;
-    private Thread visionThread;
-   // private CvSink cvSink;
-   // private CvSource outputStream; 
-   // private GripPipeline pipeline;
-   
-   public VisionController(RobotProperties properties) {
-        pipeline = new GripPipeline();
+    private Thread bwVisionThread;
+    private CvSource cvSource;
+
+    private final int width = 640;
+    private final int height = 320;
+
+    public VisionController(RobotProperties properties) {
+        bwpipeline = new GripPipeline2();
         camera1 = CameraServer.getInstance().startAutomaticCapture();
         camera1.setResolution(640, 480);
         camera2 = CameraServer.getInstance().startAutomaticCapture(1);
         camera2.setResolution(640, 480);
 
         server = CameraServer.getInstance().getServer();
-        
+
         cvSink = CameraServer.getInstance().getVideo();
+        cvSource = CameraServer.getInstance().putVideo("vision", width, height);
+
+        bwVisionThread = new Thread(() -> {
+
+            Mat img = new Mat();
+            Mat output = new Mat();
+            
+            
+            cvSink.grabFrame(img);
+            bwpipeline.process(img);
+            output = bwpipeline.desaturateOutput();
+            cvSource.putFrame(output);
+        });
+        bwVisionThread.start();
     }
 
     @Override
     public String getName() {
         return "VisionController";
     }
-               
+
     @Override
-    public boolean performAction(RobotProperties properties) 
-    {
+    public boolean performAction(RobotProperties properties) {
         cvSink.setSource(camera1);
         cvSink.setEnabled(true);
+        // System.out.println("Should show black and white");
 
         if (properties.joystick.getButtonTwo() && !prevButton) {
             server.setSource(camera2);
         } else if (!properties.joystick.getButtonTwo() && prevButton) {
             server.setSource(camera1);
         }
-        
+
         prevButton = properties.joystick.getButtonTwo();
-        
-        Mat frame = new Mat();
-
-        cvSink.grabFrame(frame);
-        
-        pipeline.process(frame);
-
-        filteredLines = pipeline.filterLinesOutput();
-
-        GripPipeline.Line line = filteredLines.get(0);
-
-        System.out.println(line.angle());
         return true;
     }
 }
