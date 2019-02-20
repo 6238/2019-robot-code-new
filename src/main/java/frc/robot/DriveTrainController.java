8b750254cf6936c75b2c42bb97d56391aee0efb0
Ishan Madan <ishan@mistlogic.com>
2019-02-19 17:57:32 -0800

    Drive Train Comments

--- src/main/java/frc/robot/DriveTrainController.java
+++ src/main/java/frc/robot/DriveTrainController.java
@@ -8,14 +8,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class DriveTrainController implements RobotController {
     
+    //speed multiplier/reducer
     double insanityFactor = 0.5;
+    
+    //inverted drive for hatch panel
     boolean reverseDrive = false;
 
     public DriveTrainController() {
+        //send values to dashboard
         SmartDashboard.putNumber("insanityFactor", insanityFactor);
         SmartDashboard.putBoolean("reverseDrive", reverseDrive);
     }
-
+    
+    //name function for initial testing
     @Override
     public String getName() {
         return "DriveTrainController";
@@ -23,9 +28,9 @@ public class DriveTrainController implements RobotController {
     
     double correctAngle; //What the robot should be doing (angle after joyZ stopped changing)
     double actualAngle; //What the gyro currently measures
-    double angleError;
-    double kError = 1/180;
-    boolean checkNextCycle;
+    double angleError; //diff btwn actual & correct
+    double kError = 1/180; //constant for sending error to actual drivecartesian function
+    boolean checkNextCycle; //check for error next cycle
 
     @Override
     public boolean performAction(RobotProperties properties) {
@@ -51,10 +56,13 @@ public class DriveTrainController implements RobotController {
 
         if (SmartDashboard.getBoolean("Joystick Control", true)) {
             if (properties.joystick.getButtonOne()) {
+                //trigger => absolute drive
                 robotDrive.driveCartesian(-insanityFactor*properties.joystick.getJoystickX(), insanityFactor*properties.joystick.getJoystickY(), -insanityFactor*properties.joystick.getJoystickZ(), actualAngle);
             } else if (reverseDrive) {
+                //reverseDrive switch
                 robotDrive.driveCartesian(insanityFactor*properties.joystick.getJoystickX(), -insanityFactor*properties.joystick.getJoystickY(), -insanityFactor*properties.joystick.getJoystickZ()/* + (angleError * kError)*/);
             } else {
+                //normal driving
                 robotDrive.driveCartesian(-insanityFactor*properties.joystick.getJoystickX(), insanityFactor*properties.joystick.getJoystickY(), -insanityFactor*properties.joystick.getJoystickZ()/* + (angleError * kError)*/);
             }
         }
