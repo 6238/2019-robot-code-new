0f61cf4561bf2fd7f28cc1d58c2f11b55796afb2
Ishan <ishan@mistlogic.com>
2019-03-22 07:39:30 -0700

    comment elevator, cargo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -33,9 +33,9 @@ public class Robot extends TimedRobot {
     properties = new RobotProperties();
     controllers = new ArrayList<RobotController>();
     controllers.add(new DriveTrainController());
-    // controllers.add(new VisionController(properties));
-    controllers.add(new ElevatorController());
-    controllers.add(new CargoController());
+    controllers.add(new VisionController(properties));
+    // controllers.add(new ElevatorController());
+    // controllers.add(new CargoController());
   }
 
   /**
